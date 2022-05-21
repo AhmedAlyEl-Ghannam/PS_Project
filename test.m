ca6cbec3840ef50784e674ed6acd52cf33e03e52
YahiaEldakhakhny <yahia4360673@gmail.com>
2022-05-21 21:30:51 +0200

    efficiency graph is working for lag pf

--- test.m
+++ test.m
@@ -1,41 +1,39 @@
+clear all
+
 % Constants
 A = -5.25+12.5i;
 B = 5+2.5i;
 C = -31.25-10.625i;
 D = A;
+
+%A=0.9706+0.00269i;
+%B=6.39+69.998i;
+%C=-1.155e-6+1i*(8.277e-4);
+%D=0.9706+0.00269i
+
 Pr = 100e3/3;
+pf = 0.3:0.01:1;
+Ir = zeros(1, length(pf));
+Vs = Ir;
+Is = Ir;
 
-% at UPF
-Vr_upf = 1000/3;
-Ir_upf = 100;
+% Get Vr from user
+Vr = 1000/3; %input("Enter Vr: ");
 
-% calculate Vs which is constant regardless of pf
-Vs = A*Vr_upf + B*Ir_upf;
+% Calculate Ir
+Ir = (Pr./(Vr.*pf)) .* exp(i.*acos(pf));
 
-% Start general calculations
-pf = 0.3:0.001:1;
-Vr = zeros(1, length(pf));
-Ir = Vr;
-Vr1 = Vr;
-Vr2 = Vr;
-Ir1 = Vr;
-Ir2 = Vr;
+% Calculate sending end values
+Vs = A*Vr + B.*Ir;
+Is = C*Vr + D.*Ir;
+Ss = Vs.*conj(Is);
+Ps = real(Ss);
 
-% Vs = A*Vr + B*Pr/(3*Vr*pf)*exp(i*acos(pf))
-% Vs*Vr = A*Vr^2 + B*Pr/(3*pf)*exp(i*acos(pf))
-for j = 1:length(pf)
-    x = B*((Pr/(pf(j)))*exp(i*acos(pf(j))));
-    Vr(j) = real(roots([A -Vs x])(1));
-    Ir(j) = (Pr/(Vr(j)*pf(j)))* exp(i*acos(pf(j)));
-
-
-endfor
-
-% Calculate Is
-Is = C*Vr + D*Ir;
-Ps = real(Vs.*conj(Is));
+% Calculating Output
 eff = Pr./Ps;
 
+% Test values
 figure
 plot(pf, eff)
 
+%Ps(find(pf==0.8))
