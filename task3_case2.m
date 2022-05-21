9f95516a64eefbb513e7fb88bce154c12494b065
Soldier-10 <kero.saadsuper@gmail.com>
2022-05-21 17:22:07 +0200

    changed stupid useless stuff so the code work on my 2013 matlab :'(

--- task3_case2.m
+++ task3_case2.m
@@ -1,6 +1,6 @@
 % Task 3 case 2
 function task3_case2(A, B, C, D, Vr_curr, Ir_curr)
-    Vs = A*Vr_curr + B*Ir_curr
+    Vs = A*Vr_curr + B*Ir_curr;
     %Vs = 20e3; % Constant
     Pr = 100e3; % power recieved constant
     pfr = 0.3:0.01:1; % power factor recieving
@@ -12,7 +12,7 @@ function task3_case2(A, B, C, D, Vr_curr, Ir_curr)
     Ir = ones(1,length(pfr));
     % Compute Vr and Ir
     for j = 1:1:length(pfr)
-        x = B*(Pr/3*pfr(j))*exp(i*acos(pfr(j)));
+        x = B*(Pr/3*pfr(j))*exp(1i*acos(pfr(j)));
         r = roots([A -Vs x]);
         r_r = real(r);
         r_i = abs(imag(r));
@@ -21,9 +21,9 @@ function task3_case2(A, B, C, D, Vr_curr, Ir_curr)
             Vr(j) = real(r(1));
         elseif abs(r(2)) > abs(r(1)) %r_i(2) < r_i(1) %r_r(2) > 0 &&
             Vr(j) = real(r(2));
-        endif
-        Ir(j) = Pr*exp(i*acos(pfr(j)))/(3*Vr(j)*pfr(j));
-    endfor
+        end
+        Ir(j) = Pr*exp(1i*acos(pfr(j)))/(3*Vr(j)*pfr(j));
+    end
 
     % Compute Is
     Is = C.*Vr + D.*Ir;
@@ -35,13 +35,13 @@ function task3_case2(A, B, C, D, Vr_curr, Ir_curr)
     %Vr
 figure
 plot(pfr, Vr)
-title("pfr vs Vr")
+title('pfr vs Vr')
 
 figure
 plot(pfr, 3.*abs(Vr).*abs(Ir).*pfr)
-title("pfr vs Pr")
+title('pfr vs Pr')
 
 figure
 plot(pfr, eff)
-title("pfr vs efficiency")
+title('pfr vs efficiency')
 endfunction
