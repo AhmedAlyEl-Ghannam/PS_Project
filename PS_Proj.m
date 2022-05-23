a4c48df915d4ccbec1754ab973405048c59766a9
AhmedAlyEl-Ghannam <ahmedaly.g112@gmail.com>
2022-05-23 11:26:51 +0200

    case 1 + 2 + kero's adjustments

--- PS_Proj.m
+++ PS_Proj.m
@@ -1,62 +1,68 @@
+%% This Project was Prepared By:
+% Ahmed Aly Gamal El-Din El-Ghannam  19015292
+% Ahmed Sherif Ahmed Mahmoud Ghanim  19015255
+% Kerollos Saad Thomas Shokrallah    19016188
+% Yahia Walid Mohamed El-Dakhakhny   19016891
+% Yosry                                N/A
+
+%% Prologue: Setup
+clear;
+clc;
+fprintf('Welcome to the Transmission Line Wizard >> \n\n');
+
 %% Task_1: Transmission Line Parameters
 
-%% A) Resistance Parameters
+%% A) Reading Values from the User
 % Storing Resistivity
-cond_res = input("Enter a Value for the Conductor's Resistivity in Ω.m:  ");
+ConResistivity = input("Enter a Value for the Conductor's Resistivity in Ohm/m:  ");
 
 % Storing Length
-con_len = input("Enter a Value for the Conductor's Length in km:  ");
+ConLength = input("Enter a Value for the Conductor's Length in km:  ");
 
 % Storing Diameter
-con_diam = input("Enter a Value for the Conductor's Diameter in m:  ");
+ConDiameter = input("Enter a Value for the Conductor's Diameter in m:  ");
 
 % Calculating Radius
-con_rad = (con_diam / 2);
+ConRadius = (ConDiameter / 2);
 
 % Calculating Conductor Length in m
-con_len_m = (con_len) * 1e3;
+ConLength_m = (ConLength) * 1e3;
 
 %% B) Inductance and Capacitance Parameters
 % Spacing Between Conductors
 spacing = read_spacing();
 
-% Idiotproofing Spacing
 while (1)
     
+    % Symmetric
     if (spacing == 0)
+       % Calculating GMD
+       GMD = input("Enter a Value for the Distance Between Conductors in m:  "); 
        break;
-    end
     
-    if (spacing == 1)
+    % Unsymmetric
+    elseif (spacing == 1)
+        D_12 = input("Enter a Value for the Distance Between Conductors One and Two in m:  ");
+        D_23 = input("Enter a Value for the Distance Between Conductors Two and Three in m:  ");
+        D_13 = input("Enter a Value for the Distance Between Conductors One and Three in m:  ");
+        % Calculating GMD
+        GMD = nthroot((D_12 * D_23 * D_13), 3);
         break;
+    % Idiotproofing Spacing
+    else
+        disp("Invalid Input! Try Again");
+        spacing = read_spacing();
     end
-    disp("Invalid Input! Try Again");
-    spacing = read_spacing();
     
-end
-        
-% Symmetric
-if (spacing == 1)
-
-    dist = input("Enter a Value for the Distance Between Conductors in m:  ");
-    
-% Unsymmetric
-else
-    
-    dist_12 = input("Enter a Value for the Distance Between Conductors One and Two in m:  ");
-    dist_23 = input("Enter a Value for the Distance Between Conductors Two and Three in m:  ");
-    dist_13 = input("Enter a Value for the Distance Between Conductors One and Three in m:  ");
-    dist_sys = dist_12 * dist_23 * dist_13;
-    dist = nthroot(dist_sym, 3);
 end
 
 %% Calculating Resistance
 
 %Calculating Area
-area = (pi / 4) * (con_diam * con_diam);
+area = (pi / 4) * (ConDiameter * ConDiameter);
 
 % DC Resistance
-R_DC = (cond_res * con_len_m ) / area;
+R_DC = (ConResistivity * ConLength_m ) / area;
 
 % AC Resistance
 R_AC = 1.1 * R_DC;
@@ -66,17 +72,15 @@ R_AC = 1.1 * R_DC;
 % Magnetic Permeability
 meu = (4 * pi) * 1e-7;
 
-% Geometric Mean Distance
-GMD = dist;
 
 % Geometric Mean Radius
-GMR = con_rad * exp(-0.25);
+GMR = ConRadius * exp(-0.25);
 
 % Inductance Per Phase
-L_phase = (meu / (2 * pi)) * log(GMD / GMR);
+L_per_m = (meu / (2 * pi)) * log(GMD / GMR);
 
 % Inductance
-L = L_phase * con_len_m;
+L = L_per_m * ConLength_m;
 
 %% Calculating Capacitance
 
@@ -84,10 +88,10 @@ L = L_phase * con_len_m;
 epsilon = 8.85e-12;
 
 % Capacitance Per Phase
-C_phase = (2 * pi * epsilon) / log(dist / con_rad);
+C_per_m = (2 * pi * epsilon) / log(GMD / ConRadius);
 
 % Capacitance
-C = C_phase * con_len_m;
+C = C_per_m * ConLength_m;
 %%%%%%%% End of Transmission Line Parameters %%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
@@ -96,7 +100,7 @@ C = C_phase * con_len_m;
 % Defining sqrt(-1) as j
 j = 1i;
 
-% Calculating ω (Assuming f = 50Hz)
+% Calculating OMEGA (Assuming f = 50Hz)
 f = 50;
 omega = 2 * pi * f;
 
@@ -113,54 +117,55 @@ Z = R_AC + XL;
 Y = (j * omega  * C);
 
 % Determining The Transmission Line Model Based on its Length
-if (con_len <= 80)
+if (ConLength <= 80)
+    % Identifying Transmission Line Model Used as Short
+    state = 'Short';
+    fprintf('Based on the line length entered. The Trasmission line is %s. \n',state);
+    
     % Short Line Parameters are Used
-    A_par = 1;
-    B_par = Z;
-    C_par = 0;
-    D_par = 1;
+    A = 1;
+    B = Z;
+    C = 0;
+    D = 1; 
     
-else
-   
-    if (con_len <= 250)
+elseif (ConLength <= 250)  
+    
+    % Identifying Transmission Line Model Used as Medium
+    state = 'Medium';
+    fprintf('Based on the line length entered. The Trasmission line is %s. \n',state);
         
-        % Medium Line Parameters are Used Based on the Circuit Model
-        model = line_model();
+    % Medium Line Parameters are Used Based on the Circuit Model
+    model = line_model();
         
-        % Idiotproofing Model
-        while (1)
-
-            if (model == 0)
-                break;
-            end
-            if (model == 1)
-                break;
-            end
-            disp("Invalid Input! Try Again");
-            model = line_model();
-
-        end
-        
-        % Switching Between Models Based on Input
-        switch (model)
-           
-            % Medium Line Parameters for π Model
-            case 1
-                A_par = 1 + (Y * Z / 2);
-                B_par = Z;
-                C_par = Y * (1 + (Y * Z / 4));
-                D_par = 1 + ((Y * Z) / 2);
+    % Idiotproofing Model
+    while (1)
+    
+      if (model == 0)
+          % Medium Line Parameters for PI Model
+          A = 1 + (Y * Z / 2);
+          B = Z;
+          C = Y * (1 + (Y * Z / 4));
+          D = 1 + ((Y * Z) / 2);
+          break;
                 
-            % Medium Line Parameters for T Model
-            case 2
-                A_par = 1 + (Y * Z / 2);
-                B_par = Z * (1 + (Y * Z / 4));
-                C_par = Y;
-                D_par = 1 + (Y * Z / 2);
+      elseif (model == 1)
+          % Medium Line Parameters for T Model
+          A = 1 + (Y * Z / 2);
+          B = Z * (1 + (Y * Z / 4));
+          C = Y;
+          D = 1 + (Y * Z / 2);
+          break;
+      else
+          disp("Invalid Input! Try Again");
+          model = line_model();
+      end
+            
+    end    
 
-        end
-    
-    end
+else
+    state = 'Long';
+    fprintf('\nBased on the line length entered. The Trasmission line is %s. \n', state);
+    fprintf('This Program is not able to calculate Long Transmission Parameters.\nTerminated ..\n');
     
 end
 
@@ -169,80 +174,14 @@ end
 
 %% Task 3: Transmission Line Performance
 
-% %% Case I
-% % Power Factor = 0.8 lag
-% pf_r = 0.8;
-% 
-% % Calculating Φ @ Receiving End
-% phi_r = acos(pf_r);
-% 
-% % Active Power Varying Between 0 and 100kW at Receiving End
-% Pr = 0 : 1 : 100000;
-% Pr_fl = 100000;
-% 
-% % Prompting the User to Enter the Receiving-end Voltage @ Full Load
-% %V_R_fl_phase = input("Enter a Value for The Receiving-End Phase Voltage at Full Load in v:  ");
-% 
-% % Prompting the User to Enter the Sending-end Voltage
-% V_S_phase = input("Enter a Value for The Sending-end Phase Voltage at Full Load in v:  ");
-% 
-% % Calculating the Value of Receiving-end Current @ Full Load
-% I_R_fl_phase = (Pr_fl / (3 * V_R_fl_phase * pf_r)) * exp(-1i*acos(0.8));
-% 
-% % Sending-end Voltage is a Constant Value Calculated by
-% %V_S_phase = (A_par * V_R_fl_phase) + (B_par * I_R_fl_phase);
-% 
-% % Receiving-end Voltage @ No Load is
-% V_R_nl_phase = V_S_phase / A_par;
-% 
-% % Defining Receiving-end Voltage as an Array Ranging from V_R_nl_phase to V_R_fl_phase
-% V_R_phase = abs(V_R_nl_phase) : 1 : abs(V_R_fl_phase);
-% 
-% % Defining Receiving-end Current as an Array Ranging from 0 to I_R_fl_phase
-% I_R_phase = (Pr ./ ((3 * pf_r) .* V_R_phase)) * exp(1i * acos(pf_r));
-% 
-% % Calculating Sending-end Current 
-% I_S_phase = (C_par .* V_R_phase) + (D .* I_R_phase);
-% 
-% % Calculating the Power Factor of Sending-end
-% phi_s = -angle(I_S_phase);
-% 
-% % Calculating Sending-end Active Power
-% 
-% 
-% 
-% 
-% %%%%%%%%%%%%%%%%%%%%%
-% 
-% % Trying Voltage Regulation Curve
-% 
-% %VR = (abs(V_R_nl_phase) - abs(V_R_fl_phase)) / (V_R_fl_phase);
-% 
-% %figure
-% %plot(VR, Pr)
-% 
-% 
-% 
-% 
-% % Calculating the Receiving-end Current as an Array
-% 
-% 
-% 
-% 
-% 
-% 
+% Prologue : Prompting the User to Enter the Receiving-end Voltage
+Vr = input('Enter a Value for The Receiving-End Phase Voltage in v:  ');
 
+% Case I
+task3_case1(A, B, C, D, Vr, j);
 
-
-
-
-
-
-
-
-
-
-
+% Case II
+task3_case2(A, B, C, D, Vr, j);
 
 %%%%%%%%%%% Transmission Line Performance %%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -251,7 +190,7 @@ end
 
 % Function that Reads Spacing Between Conductors
 function spac = read_spacing()
-    spac = input("Enter 1 for Symmetric Spacing and 0 for Unsymmetric Spacing:  "); 
+    spac = input("Enter 0 for Symmetric Spacing and 1 for Unsymmetric Spacing:  "); 
 end
 
 %Function that Reads the User's Choice between T and π Models
@@ -259,8 +198,123 @@ function mod = line_model()
     mod = input("Enter 1 for π Model and 0 for T Model:  "); 
 end
 
+function task3_case1(A, B, C, D, Vr, j)
+    %% Initialize Power Factor (Constant, Given)
+    pf = 0.8;
+    
+    %% Active Power Varying Between 0 and 100kW at Receiving End
+    Pr = 0:0.5:100;
+    % Measuring Active Power in W and Dividing by 3 to get Power per Phase
+    Pr = Pr.*1000/3;
+   
+    %% Calculating the Value of Receiving-end Current
+    Ir = (Pr ./ ( Vr * pf)) * exp(j * acos(pf));
+    
+    %% Calculating Sending-end Values
+    Vs = (A * Vr) + (B .* Ir);
+    Is = (C * Vr) + (D .* Ir);
+    Ss = Vs .* conj(Is);
+    Ps = real(Ss);
+    
+    %% Calculating Efficiency
+    eff = Pr ./ Ps;
+    
+    %% Calculating Receiving-end Voltage @ no load
+    Vr_nl = Vs ./ A;
+    
+    %% Calculating Voltage Regulation
+    V_R = (abs(Vr_nl) - Vr) ./ Vr;
+    
+    %% Measuring Active Power in kW and Reverting it to its Original Three-phase State 
+    Pr = Pr.*3/1000;
+    
+    %% Graphs
+    % Plotting Efficiency in % vs Active Power in kW
+    figure
+    subplot(121)
+    plot(Pr, eff.*100); 
+    grid on
+    title('Efficiency (%) vs Active Power (kW)')
+    
+    % Plotting Voltage Regulation (%) vs Active Power
+    subplot(122)
+    plot(Pr, V_R.*100); 
+    grid on
+    title('Plotting Voltage Regulation (%) vs Active Power')
+    
+end
 
+% Function Used to Plot the Graphs Required in Task 3 Case II
+function task3_case2(A, B, C, D, Vr, j)
+    %% Defining Active Power @ Receiving End
+    Pr = 100e3/3;
+    
+    %% Defining Power Factor Ranging between 0.3 and 1
+    pf = 0.3:0.01:1;
 
+    %% Calculating the Value of Receiving-end Current
+    Ir_lag = (Pr ./ (Vr .* pf)) .* exp(j .* acos(pf));
+    Ir_lead = (Pr ./ (Vr .* pf)) .* exp(-j .* acos(pf));
+    
+    %% Calculations @ Lagging Power Factor
+    % Calculating Sending-End Values
+    Vs_lag = (A * Vr) + (B .* Ir_lag);
+    Is_lag = (C * Vr) + (D .* Ir_lag);
+    Ss_lag = Vs_lag .* conj(Is_lag);
+    Ps_lag = real(Ss_lag);
+    
+    % Calculating Efficiency
+    eff_lag = Pr./Ps_lag; 
+    
+    % Calculating Receiving-end Voltage @ no load
+    Vrnl_lag = Vs_lag./A; 
+    
+    % Calculating Voltage Regulation
+    V_R_lag = (abs(Vrnl_lag) - Vr)./Vr;
+    
+    %% Calculations @ Leading Power Factor
+    % Calculating Sending-End Values
+    Vs_lead = A*Vr + B.*Ir_lead;
+    Is_lead = C*Vr + D.*Ir_lead;
+    Ss_lead = Vs_lead.*conj(Is_lead);
+    Ps_lead = real(Ss_lead);
+    
+    % Calculating Efficiency
+    eff_lead = Pr./Ps_lead;
+    
+    % Calculating Receiving-end Voltage @ no load
+    Vrnl_lead = Vs_lead./A; 
+    
+    % Calculating Voltage Regulation
+    V_R_lead = (abs(Vrnl_lead) - Vr)./Vr;
+    
+    %% Graphs @ Lagging Power Factor
+    % Plotting Efficiency vs Power Factor 
+    figure
+    subplot(221)
+    plot(pf, eff_lag)
+    grid on
+    title("Efficiency vs Lagging PF")
+
+    % Plotting Voltage Regulation vs Power Factor
+    subplot(222)
+    plot(pf, V_R_lag)
+    grid on
+    title("Voltage Regulation vs Lagging")
+    
+    %% Graphs @ Leading Power Factor
+    % Plotting Efficiency vs Power Factor
+    subplot(223)
+    plot(pf, eff_lead)
+    grid on
+    title("Efficiency vs Leading PF")
+    % Plotting Voltage Regulation vs Power Factor
+    subplot(224)
+    plot(pf, V_R_lead)
+    grid on
+    title("Voltage Regulation vs Leading PF")
+    
+end
 
 
 
