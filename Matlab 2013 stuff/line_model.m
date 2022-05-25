fc3c9ce61c15c73b6c8e74054e45eb3c0f9c10c3
YahiaEldakhakhny <yahia4360673@gmail.com>
2022-05-25 19:37:36 +0200

    Removed unneeded files

--- Matlab 2013 stuff/line_model.m
+++
@@ -1,4 +0,0 @@
-%% Function that Reads the User's Choice between T and π Models
-function mod = line_model()
-    mod = input('Enter 1 for π Model and 0 for T Model:  '); 
-end
--- Matlab 2013 stuff/read_spacing.m
+++
@@ -1,4 +0,0 @@
-%%
-function spac = read_spacing()
-    spac = input('Enter 0 for Symmetric Spacing and 1 for Unsymmetric Spacing:  '); 
-end
